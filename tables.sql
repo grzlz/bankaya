5aead5774a7de2c8bafaea1cae6d0dc36624d944
grzlz <guillermo.rdzlpz@gmail.com>
2023-03-09 17:06:28 -0500

    begin sql file

---
+++ tables.sql
@@ -0,0 +1,7 @@
+Customer: This table will contain a primary key as well as the customers firsta and last
+names, phone number, CURP, RFC, and address information
+Items: This table will contain a primary key the item name, and the item price
+Items Bought: This table will contain an order number, date, price and comments. It will
+also connect to the primary keys in the Items and Customer Tables.
+
+CREATE TABLE 
