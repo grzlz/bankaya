4923a9d926f7225bb4c8ddda1e5cb20c8913c7d4
grzlz <guillermo.rdzlpz@gmail.com>
2023-03-09 18:42:27 -0500

    create tables

--- tables.sql
+++ tables.sql
@@ -1,7 +1,24 @@
-Customer: This table will contain a primary key as well as the customers firsta and last
-names, phone number, CURP, RFC, and address information
-Items: This table will contain a primary key the item name, and the item price
-Items Bought: This table will contain an order number, date, price and comments. It will
-also connect to the primary keys in the Items and Customer Tables.
+CREATE TABLE customer_tbl (
+    customer_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    phone_number VARCHAR(13) NOT NULL,
+    curp VARCHAR(18) NOT NULL UNIQUE,
+    rfc VARCHAR(13) NOT NULL UNIQUE,
+    address VARCHAR NOT NULL
+    );
 
-CREATE TABLE 
+CREATE TABLE item_tbl (
+    item_id SERIAL PRIMARY KEY,
+    item_name VARCHAR(30) NOT NULL,
+    item_price NUMERIC(7, 2) NOT NULL
+    );
+
+CREATE TABLE item_purchase_tbl (
+    order_id SERIAL PRIMARY KEY,
+    purchase_date DATE NOT NULL,
+    purchase_price NUMERIC(7, 2) NOT NULL,
+    comments VARCHAR,
+    item_id INT NOT NULL REFERENCES item_tbl (item_id),
+    customer_id INT NOT NULL REFERENCES customer_tbl (customer_id)
+    );
